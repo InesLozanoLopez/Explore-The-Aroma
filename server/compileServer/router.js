ba1147e5ca78ed78aa88f6a836e1443ea237bbc6
Ines <lozano.lopez.ines@gmail.com>
2023-06-20 11:12:15 +0100

    untracking files

--- server/compileServer/router.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-import { Router } from 'express';
-import controller from './controllers/controllers.js';
-const router = Router();
-router.get('/parfumes', controller.getAll);
-export default router;
